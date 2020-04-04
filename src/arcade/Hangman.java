e18f47605f1af018f85493e62fce461f658490c2
awengca <andrewweng@wustl.edu>
2020-04-03 18:02:57 -0700

    noose implemented

--- src/arcade/Hangman.java
+++ src/arcade/Hangman.java
@@ -5,25 +5,25 @@ public class Hangman {
     public static void main(String [] args){
         int numWrong = 0;
         for(int i = 0; i <= 6; i++){
-            draw(i);
-            System.out.println("_________");
+            drawNumWrongGuesses(i);
+            System.out.println(" ");
         }
     }
 
-    public static void draw(int numWrong){
+    public static void drawNumWrongGuesses(int numWrong){
         String printable = "";
         if(numWrong == 1){
-            printable = "\t O\t\n";
+            printable = "|====|\n|\t @\t\n|\n|\n|\n|_________";
         }else if(numWrong ==2){
-            printable = "\t O\t\n\t |\n";
+            printable = "|====|\n|\t @\t\n|\t |\n|\n|\n|_________";
         }else if(numWrong ==3 ){
-            printable = "\t O\t\n\t/|\n";
+            printable = "|====|\n|\t @\t\n|\t/|\n|\n|\n|_________";
         }else if(numWrong ==4){
-            printable = "\t O\t\n\t/|\\\n";
+            printable = "|====|\n|\t @\t\n|\t/|\\\n|\n|\n|_________";
         }else if(numWrong ==5){
-            printable = "\t O\t\n\t/|\\\n\t/\n";
+            printable = "|====|\n|\t @\t\n|\t/|\\\n|\t/ \n|\n|_________";
         }else if(numWrong ==6){
-            printable = "\t O\t\n\t/|\\\n\t/ \\\n YOU LOST\n";
+            printable = "|====|\n|\t @\t\n|\t/|\\\n|\t/ \\\n|\n|_________\n\n  YOU LOST\n";
         }
         System.out.print(printable);
     }
