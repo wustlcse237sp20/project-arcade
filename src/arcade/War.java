5245bfde0917a0c2c10283d7fbe2721ca76b2382
Anya Pawar <anya.pawar@wustl.edu>
2020-04-03 20:49:24 -0400

    commit- anya

--- src/arcade/War.java
+++ src/arcade/War.java
@@ -1,7 +1,12 @@
 package arcade;
 
 public class War {
+	public static void main(String args[]){
+		System.out.println("Welcome to the game of War");
+		
+	}
 	
-	//anya is testing something
-	//more testing
-}
+	
+	}
+	
+
