e715010e87e75d2a46702937487f957a90353fd3
Kati Gabbard <katherine.kate@wustl.edu>
2020-04-28 14:08:08 -0500

    added games statistics

--- src/arcade/Connect4.java
+++ src/arcade/Connect4.java
@@ -3,8 +3,14 @@ package arcade;
 //import java.awt.SystemTray;
 
 public class Connect4 {
+	public static boolean uiWinStatus = false;
+	
     public static void main(){
     	System.out.println("Connect 4 ran");
     }
+    
+    public static boolean uiCheckWinStatus() {
+		return uiWinStatus;
+    } 
 
 }
