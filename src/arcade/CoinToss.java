2422ed4cffe62b4cf9cab742b9414f5a9a63cd82
Anya Pawar <anya.pawar@wustl.edu>
2020-05-02 20:54:55 -0400

    working game - coin toss

--- src/arcade/CoinToss.java
+++ src/arcade/CoinToss.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 
 
 public class CoinToss {
-	 
-public static int comp = 0; 
+
+public static int playerVal = 0;
+public static int randomVal = 0; 
 public static int heads = 1;
 public static int tails = 2; 
 
@@ -17,17 +18,17 @@ public static void main(String args[]){
 }
 
 public static void check(){
-	comp = (int)(Math.random() * ((2 - 1) + 1)) + 1;	
+	randomVal = (int)(Math.random() * ((2 - 1) + 1)) + 1;	
 	String object= keyboard.next();	
 	if(object.equals("h")) {
-		choiceH(object);
-		System.out.println("You choose Heads! \nComputer is Tails!");
-		System.out.println("Lets Toss!\n...");
+		System.out.println("You choose Heads\nComputer is Tails");
+		playerVal = heads;
+		winnerPrint(object);
 	}
 	else if(object.equals("t")) {
-		choiceT(object);
-		System.out.println("You choose Tails! \nComputer is Heads!");
-		System.out.println("Lets Toss!\n...");
+		System.out.println("You choose Tails\nComputer is Heads");
+		playerVal = tails;
+		winnerPrint(object);
 	}
 	else {
 		System.out.println("Oops! Please enter either heads (h) or tails (t)!");
@@ -35,12 +36,28 @@ public static void check(){
 	}
 }
 
-public static void choiceH(String object) {
-	
+public static void winningCoinPrint(){
+	System.out.println("Lets Toss...\n");
+	System.out.println("The winning coin face is...");
+	if(randomVal == heads) {
+		System.out.println("HEADS");
+	}
+	if(randomVal == tails) {
+		System.out.println("TAILS");
+	}
 }
-public static void choiceT(String object) {
-	
+
+public static void winnerPrint(String object) {
+	winningCoinPrint(); 
+	if(randomVal == playerVal) {
+		System.out.println("YOU WIN");
+	}
+	else if(randomVal != playerVal) {
+		System.out.println("You lose");
+	}
 }
+
+
 
 	
 
